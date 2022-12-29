9acb6f855a4120c6677a9838a3bf581100db6450
Alexandre Nicolaie dit Clairville <alexandre.nicolaie@radiofrance.com>
2022-12-29 17:49:03 +0100

    feat: add cmd package

---
+++ cmd/gitlab-ci-pipelines-exporter/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"gitlab-ci-pipelines-exporter/pkg/webhook"
+
+	"github.com/urfave/cli/v2"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var version = "devel"
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "gitlab-ci-pipelines-exporter"
+	app.Usage = "Export metrics about GitLab CI pipelines statuses"
+	app.Version = version
+	app.EnableBashCompletion = true
+
+	app.Flags = cli.FlagsByName{
+		&cli.StringFlag{
+			Name:    "web.listen-address",
+			Usage:   "address:port to listen on for telemetry",
+			Value:   ":9252",
+			EnvVars: []string{"WEB_LISTEN_ADDRESS"},
+		},
+		&cli.StringFlag{
+			Name:    "web.telemetry-path",
+			Usage:   "Path under which to expose metrics",
+			Value:   "/metrics",
+			EnvVars: []string{"WEB_TELEMETRY_PATH"},
+		},
+		&cli.StringFlag{
+			Name:     "gitlab.webhook-secret-token",
+			Usage:    "`token` used to authenticate legitimate requests (overrides config file parameter)",
+			Required: true,
+			EnvVars:  []string{"GITLAB_WEBHOOK_SECRET_TOKEN"},
+		},
+		&cli.StringFlag{
+			Name:    "log.level",
+			Usage:   "Log verbosity",
+			Value:   "info",
+			EnvVars: []string{"LOG_LEVEL"},
+		},
+		&cli.BoolFlag{
+			Name:   "dev",
+			Hidden: true,
+		},
+	}
+
+	app.Action = func(ctx *cli.Context) error {
+		lvl, err := zapcore.ParseLevel(ctx.String("log.level"))
+		if err != nil {
+			return fmt.Errorf("failed to parse log.level: %w", err)
+		}
+
+		var logger *zap.Logger
+		if ctx.Bool("dev") {
+			logger, err = zap.NewDevelopment(zap.IncreaseLevel(lvl))
+		} else {
+			logger, err = zap.Config{
+				Level:       zap.NewAtomicLevelAt(lvl),
+				Development: false,
+				Sampling: &zap.SamplingConfig{
+					Initial:    100,
+					Thereafter: 100,
+				},
+				Encoding: "json",
+				EncoderConfig: zapcore.EncoderConfig{
+					TimeKey:        "ts",
+					LevelKey:       "level",
+					NameKey:        "logger",
+					CallerKey:      "caller",
+					FunctionKey:    zapcore.OmitKey,
+					MessageKey:     "msg",
+					StacktraceKey:  "stacktrace",
+					LineEnding:     zapcore.DefaultLineEnding,
+					EncodeLevel:    zapcore.LowercaseLevelEncoder,
+					EncodeTime:     zapcore.RFC3339NanoTimeEncoder,
+					EncodeDuration: zapcore.SecondsDurationEncoder,
+					EncodeCaller:   zapcore.ShortCallerEncoder,
+				},
+				OutputPaths:      []string{"stderr"},
+				ErrorOutputPaths: []string{"stderr"},
+			}.Build()
+		}
+		if err != nil {
+			return err
+		}
+
+		logger.Info(fmt.Sprintf("start listening on %s", ctx.String("web.listen-address")))
+		return http.ListenAndServe(
+			ctx.String("web.listen-address"),
+			webhook.NewWebhook(
+				ctx.String("web.telemetry-path"),
+				ctx.String("gitlab.webhook-secret-token"),
+				webhook.WithZapLogger(logger),
+			),
+		)
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/prometheus/client_golang v1.14.0
 	github.com/stretchr/testify v1.8.1
+	github.com/urfave/cli/v2 v2.23.7
 	github.com/urfave/negroni v1.0.0
 	go.uber.org/zap v1.24.0
 )
@@ -12,6 +13,7 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
@@ -19,6 +21,8 @@ require (
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
